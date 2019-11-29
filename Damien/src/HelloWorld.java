a64b0359078306efbdd0d59408bdd05c5be95b37
skolsen3 <34397333+skolsen3@users.noreply.github.com>
2019-11-29 12:21:47 +0100

    Jeg har opgraderet programmet

--- Damien/src/HelloWorld.java
+++ Damien/src/HelloWorld.java
@@ -1,5 +1,8 @@
+import java.sql.SQLOutput;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World! My Name is...");
+        System.out.println("Sammen med min ugle Hedvig den lille skank");
     }
 }
