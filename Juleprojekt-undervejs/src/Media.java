4670deff95a74f107c79f31ca2ae423a231ad7f1
bluetooth-afk <damr@itu.dk>
2019-12-04 12:50:04 +0100

    tjo

--- Juleprojekt-undervejs/src/Media.java
+++ Juleprojekt-undervejs/src/Media.java
@@ -1,11 +1,11 @@
 public class Media {
     protected String name;
     protected String year;
-    protected double rating;
     protected String[] genres;
+    protected double rating;
     protected boolean visible;
 
-    public Media(String name, String year, double rating, String[] genres, boolean visible) {
+    public Media(String name, String year, String[] genres, double rating, boolean visible) {
         this.name = name;
         this.year = year;
         this.rating = rating;
