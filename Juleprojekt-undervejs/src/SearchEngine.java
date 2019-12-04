5eb534e0d13bc15c98b527b8813d37d82471304d
skolsen3 <34397333+skolsen3@users.noreply.github.com>
2019-12-04 14:27:50 +0100

    Ændringer til Seach Engine
    SearchEngine kan nu både sortere efter kategorier og søge efter tekstStrenge

--- Juleprojekt-undervejs/src/SearchEngine.java
+++ Juleprojekt-undervejs/src/SearchEngine.java
@@ -4,12 +4,51 @@ public class SearchEngine {
     protected Library library;
 
     public ArrayList<Media> searchForString(String s) {
-        ArrayList<Media> tempList = new ArrayList<Media>();
+        ArrayList<Media> tempList = new ArrayList<>();
         ArrayList<Media> l = library.getAsList();
         for (Media m : l) {
             if (m.getName().contains(s)) {
                 tempList.add(m);
             }
+
+        }
+        return tempList;
+    }
+
+    public ArrayList<Media> searchByCategory(String category1) {
+        ArrayList<Media> tempList = new ArrayList<>();
+        ArrayList<Media> l = library.getAsList();
+        for (Media m : l) {
+            if (m.getGenres().toString().contains(category1)) {
+                tempList.add(m);
+            }
+
+        }
+        return tempList;
+    }
+
+    public ArrayList<Media> searchByCategory(String category1, String category2) {
+        ArrayList<Media> tempList = new ArrayList<>();
+        ArrayList<Media> l = library.getAsList();
+        for (Media m : l) {
+            if (m.getGenres().toString().contains(category1))
+                if (m.getGenres().toString().contains(category2)) {
+                    tempList.add(m);
+                }
+
+        }
+        return tempList;
+    }
+    public ArrayList<Media> searchByCategory(String category1, String category2, String category3) {
+        ArrayList<Media> tempList = new ArrayList<>();
+        ArrayList<Media> l = library.getAsList();
+        for (Media m : l) {
+            if (m.getGenres().toString().contains(category1))
+                if (m.getGenres().toString().contains(category2))
+                if(m.getGenres().toString().contains(category3))
+                {
+                    tempList.add(m);
+                }
 
         }
         return tempList;
