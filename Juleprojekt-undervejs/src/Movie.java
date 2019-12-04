0210bbad74776947d1bd89c5667fc1af75522a39
bluetooth-afk <damr@itu.dk>
2019-12-04 14:04:28 +0100

    Opdateret filereader med tester

--- Juleprojekt-undervejs/src/Movie.java
+++ Juleprojekt-undervejs/src/Movie.java
@@ -1,6 +1,6 @@
 public class Movie extends Media {
 
-    public Movie(String name, String year, String[] genres, double rating, boolean visible) {
+    public Movie(String name, String year, String[] genres, String rating, boolean visible) {
         super(name, year, genres, rating, visible);
     }
 }
