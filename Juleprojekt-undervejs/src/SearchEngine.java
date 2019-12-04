81691e7cff53501687c4fb39a8520be91732d56d
skolsen3 <34397333+skolsen3@users.noreply.github.com>
2019-12-04 13:51:50 +0100

    Småting, lidt på Search engine

--- Juleprojekt-undervejs/src/SearchEngine.java
+++ Juleprojekt-undervejs/src/SearchEngine.java
@@ -1,4 +1,15 @@
+import java.util.ArrayList;
+
 public class SearchEngine {
     protected Library library;
-    
-}
+
+    public Library searchForString(String s){
+        ArrayList<Media> tempList = new ArrayList<Media>();
+        for(Media m : library){
+            if(m.getName)
+
+        }
+
+
+    }
+    }
