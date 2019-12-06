1f9e109bd774eddf66a2fee3bfa02d25cd3be57e
skolsen3 <34397333+skolsen3@users.noreply.github.com>
2019-12-06 12:08:50 +0100

    "Easter eggs" i view Kapp

--- Juleprojekt-undervejs/src/View.java
+++ Juleprojekt-undervejs/src/View.java
@@ -1,3 +1,18 @@
 public class View {
+private int sebastianIQ = 140;
+private int tobiasIQ = 140;
+private int damienIQ = 80;
 
+public void PrintHead(){
+    if(sebastianIQ>=130){
+        System.out.println("5Head");
+    }
+    if(tobiasIQ>=130){
+        System.out.println("5Head");
+    }
+    if(damienIQ<=85){
+        System.out.println("3Head");
+    }
+
+}
 }
