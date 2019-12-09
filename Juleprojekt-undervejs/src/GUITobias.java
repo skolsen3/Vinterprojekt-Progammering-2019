701f3be61643ad3e77629397b39c9e7b5329208c
tobias-birch <tbir@itu.dk>
2019-12-09 09:40:17 +0100

    simpel northPanel og southPanel

--- Juleprojekt-undervejs/src/GUITobias.java
+++ Juleprojekt-undervejs/src/GUITobias.java
@@ -3,14 +3,31 @@ import java.awt.*;
 
 
 public class GUITobias {
-    private JFrame frame;
-    public void makeFrame() {
-        frame = new JFrame("playIT");
+    public static void main(String[] args) {
+        makeFrame();
+    }
+
+    public static void makeFrame() {
+        JFrame frame = new JFrame("playIT");
 
         Container contentPane = frame.getContentPane();
         contentPane.setLayout(new BorderLayout());
         //NORTH
-        contentPane.add(new JPanel(), BorderLayout.NORTH);
+        JPanel northPanel = new JPanel(new FlowLayout(FlowLayout.TRAILING));
+        JButton myListButton = new JButton("Min Liste");
+        JButton seriesButton = new JButton("Serier");
+        JButton movieButton = new JButton("Film");
+        JTextField searchField = new JTextField(16);
+        JButton searchButton = new JButton("?");
+        JButton userProfileButton = new JButton("(Bruger)");
+        northPanel.add(myListButton);
+        northPanel.add(seriesButton);
+        northPanel.add(movieButton);
+        northPanel.add(searchField);
+        northPanel.add(searchButton);
+        northPanel.add(userProfileButton);
+
+        contentPane.add(northPanel, BorderLayout.NORTH);
 
 
         //WEST
@@ -25,7 +42,10 @@ public class GUITobias {
 
 
         //SOUTH
-        contentPane.add(new JPanel(), BorderLayout.SOUTH);
+        JPanel southPanel = new JPanel(new FlowLayout(FlowLayout.TRAILING));
+        JLabel rightsLabel = new JLabel("ALL RIGHTS RESERVED. TM & COPYRIGHT");
+        southPanel.add(rightsLabel);
+        contentPane.add(southPanel, BorderLayout.SOUTH);
 
 
         frame.pack();
