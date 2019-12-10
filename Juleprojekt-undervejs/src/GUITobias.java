af98a69a3391785c361802661c663cda1daafe32
tobias-birch <tbir@itu.dk>
2019-12-10 19:57:08 +0100

    simpel login skærm
    har lavet et bud på en login skærm. Bare med felter til brugernavn og password + login knap.

--- Juleprojekt-undervejs/src/GUITobias.java
+++ Juleprojekt-undervejs/src/GUITobias.java
@@ -1,6 +1,56 @@
-/*import javax.swing.*;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 
+//Har lavet et frame, som fungerer som en login skærm. Der er et felt til at skrive brugernavn, og et felt til adgangskoden.
+//
+
+public class GUITobias{
+
+    public static void main (String[] args) {
+        JFrame frame = new JFrame("skrt");
+        Container contentPane = frame.getContentPane();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        contentPane.setLayout(new FlowLayout(FlowLayout.CENTER,5,5));
+        JPanel leftColumn = new JPanel(new GridLayout(2,1,5,10));
+        JPanel rightColumn = new JPanel(new GridLayout(2,1,5,5));
+        JLabel userLabel = new JLabel("Brugernavn:");
+        JLabel passLabel = new JLabel("Password:");
+        JTextField usernameField = new JTextField();
+        JTextField passwordField = new JTextField();
+        JButton loginButton = new JButton("Login");
+        leftColumn.add(userLabel);
+        leftColumn.add(passLabel);
+        rightColumn.add(usernameField);
+        rightColumn.add(passwordField);
+
+        // lidt visuelle tilføjelser.
+        usernameField.setColumns(20);
+        passwordField.setColumns(20);
+        usernameField.setBorder(new LineBorder(Color.DARK_GRAY,1));
+        passwordField.setBorder(new LineBorder(Color.DARK_GRAY,1));
+        leftColumn.setBackground(Color.LIGHT_GRAY);
+        rightColumn.setBackground(Color.LIGHT_GRAY);
+        contentPane.setBackground(Color.LIGHT_GRAY);
+
+        contentPane.add(leftColumn);
+        contentPane.add(rightColumn);
+        contentPane.add(loginButton);
+
+        frame.setSize(1000,1000);
+        frame.setVisible(true);
+
+        // hvis man klikker på login og har indtastet de rigtige oplysninger bliver vinduet usynligt
+        loginButton.addActionListener(e -> {
+            if(usernameField.getText().equals("Dankmemer420") && passwordField.getText().equals("Pepega")){
+                frame.setVisible(false);
+            }
+        });
+    }
+}
+/*import javax.swing.*;
+import java.awt.*;
 
 public class GUITobias {
     public static void main(String[] args) {
@@ -12,7 +62,10 @@ public class GUITobias {
 
         Container contentPane = frame.getContentPane();
         contentPane.setLayout(new BorderLayout());
+
+
         //NORTH
+        // Laver et simpelt flowlayout panel med nogle de nødvendige buttons.
         JPanel northPanel = new JPanel(new FlowLayout(FlowLayout.TRAILING));
         JButton myListButton = new JButton("Min Liste");
         JButton seriesButton = new JButton("Serier");
