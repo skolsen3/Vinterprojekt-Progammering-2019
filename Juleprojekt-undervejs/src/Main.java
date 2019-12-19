938503f1346dbdf5b691ee8e06f11183e0d6ef36
bluetooth-afk <damr@itu.dk>
2019-12-19 17:33:18 +0100

    java doc

--- Juleprojekt-undervejs/src/Main.java
+++ Juleprojekt-undervejs/src/Main.java
@@ -1,5 +1,11 @@
+/**
+ * This is the main-class of the Streaming Service playIT.
+ * 
+ * @version 1.0 Date dd-mm-yyyy: 20-12-2019
+ * @author Damien Rolighed, Sebastian Krogh Olsen, Tobias Birch
+ */
 public class Main {
     public static void main(String[] args) {
-        Controller c = new Controller();
+        new Controller();
     }
 }
